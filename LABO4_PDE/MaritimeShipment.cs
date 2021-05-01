4db22cd0b42247ab9e44af3a98b25fd71ca87180
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-30 22:58:21 -0400

    Create MaritimeShipment.cs

---
+++ LABO4_PDE/MaritimeShipment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABO4_PDE_BRIDGE
+{
+    public class MaritimeShipment : ISend
+    {
+
+        public void package(string deliverPackage, string pickUpPackage)
+        {
+
+            Console.WriteLine("The package was shipped by sea: \n{0}\n{1}\n", deliverPackage, pickUpPackage);
+
+        }
+
+    }
+}
