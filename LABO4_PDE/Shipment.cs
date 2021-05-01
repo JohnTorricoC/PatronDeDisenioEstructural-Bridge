4cc9db230a55f381540949b1d735607593ea9612
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-30 22:58:36 -0400

    Create Shipment.cs

---
+++ LABO4_PDE/Shipment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABO4_PDE_BRIDGE
+{
+
+     public abstract class Shipment
+     {
+        public ISend ShipmentSender { get; set; } //MessageSender
+        public string PickUpPackage { get; set; } //Subject
+        public string DeliverPackage { get; set; } //Body
+        public abstract void Send();       
+     }
+   
+
+}
