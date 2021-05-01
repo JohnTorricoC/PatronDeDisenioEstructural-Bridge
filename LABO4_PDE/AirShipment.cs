4d44d9cc3b1b2919f4b81dbe034f11b8ca1fde71
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-30 22:58:08 -0400

    Create AirShipment.cs

---
+++ LABO4_PDE/AirShipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace LABO4_PDE_BRIDGE
+{
+    public class AirShipment : ISend
+    {
+
+        public void package(string deliverPackage, string pickUpPackage)
+        {
+
+            Console.WriteLine("The package was shipped by air: \n{0}\n{1}\n", deliverPackage, pickUpPackage);
+
+        }
+
+    }
+}
