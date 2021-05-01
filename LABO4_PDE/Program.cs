c45394d871c58e7086860de0cc5c6df0f9af6f7d
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-30 22:58:33 -0400

    Create Program.cs

---
+++ LABO4_PDE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LABO4_PDE_BRIDGE
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ISend airShipment = new AirShipment();
+            ISend maritimeShipment = new MaritimeShipment();
+
+            Shipment shipment = new SystemShipment();
+            shipment.DeliverPackage = "Package successfully delivered.";
+            shipment.PickUpPackage = "package satisfactorily collected.";
+
+            shipment.ShipmentSender = airShipment;
+            shipment.Send();
+
+            shipment.ShipmentSender = maritimeShipment;
+            shipment.Send();
+
+            UserEvaluation userComment = new UserEvaluation();
+            userComment.DeliverPackage = "Package delivered. ";
+            userComment.PickUpPackage = "Package picked up. ";
+            userComment.UserComments = "Product collected in very good condition, shipping 10/10";
+
+            userComment.ShipmentSender = airShipment;
+            userComment.Send();
+
+            Console.ReadKey();
+        }
+    }
+}
