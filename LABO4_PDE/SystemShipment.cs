22b2da0b0f6d78b64434cbc37330e2c8285d2fe9
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-30 22:58:41 -0400

    Create SystemShipment.cs

---
+++ LABO4_PDE/SystemShipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABO4_PDE_BRIDGE
+{
+    public class SystemShipment : Shipment
+    {
+        public override void Send()
+        {
+            ShipmentSender.package(DeliverPackage, PickUpPackage);
+        }
+    }
+}
