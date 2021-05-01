46527786e67fef97ee6f6c62b6a87ec4412efba2
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-30 22:58:45 -0400

    Create UserEvaluation.cs

---
+++ LABO4_PDE/UserEvaluation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABO4_PDE_BRIDGE
+{
+    public class UserEvaluation : Shipment
+    {
+        public string UserComments { get; set; }
+
+        public override void Send()
+        {
+            string fullBody = string.Format("{0}\nUser Comments: {1}", DeliverPackage, UserComments);
+            ShipmentSender.package(PickUpPackage, fullBody);
+        }
+    }
+}
