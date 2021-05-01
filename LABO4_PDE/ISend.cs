5e6be34a3f0bb1ce4ca179af6118e602752e2f85
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-30 22:58:15 -0400

    Create ISend.cs

---
+++ LABO4_PDE/ISend.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LABO4_PDE_BRIDGE
+{
+    public interface ISend
+    {
+        void package(string deliverPackage, string pickUpPackage);
+    }
+}
